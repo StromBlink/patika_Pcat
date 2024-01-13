8ce178100fcee5624403641d6aab6b6bebb002bf
Fatih Erdem <109786091+StromBlink@users.noreply.github.com>
2024-01-13 18:51:02 +0300

    'express & nodemon' added

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const fn = function () {
-    console.log('Function Working');
-};
+const express = require('express')
+
+const app = express();
+
+app.get(`/`, (req, res) => {
+
+    const photo = {
+        id: 1,
+        name: "Photo Name",
+        descripton: "photo descripton"
+    }
+    res.send(photo)
+})
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`${port} port started`);
+});
+
